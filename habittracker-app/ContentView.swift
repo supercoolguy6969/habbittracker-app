397624aa6bf0aacf6cbcd82ea0b0fa494a6ae78c
ankar kieram <ankar3008@gmail.com>
2023-10-01 12:20:49 -0400

    initial commit

---
+++ habittracker-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  habittracker-app
+//
+//  Created by Ankar Kieram on 2023-10-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ habittracker-app/habittracker_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  habittracker_appApp.swift
+//  habittracker-app
+//
+//  Created by Ankar Kieram on 2023-10-01.
+//
+
+import SwiftUI
+
+@main
+struct habittracker_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
